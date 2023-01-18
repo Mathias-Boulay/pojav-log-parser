805572c7edc5f8f3d53ca8fd536b369b98bc4c4c
Boulay Mathias <mathiasboulay@free.fr>
2023-01-18 20:20:09 +0100

    Feat: add a basic error checker
    This will look for oddities in the log file

---
+++ src/locale.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+
+en_locale = {
+    'mipmap.enabled': 'Mipmap is not disabled, this may destroy performance',
+    'libgles.wrong.value': 'Improper LIBGL_ES value !',
+    'libgl.custom.value': 'Custom openGL declaration !',
+    'java.args.unparsed': 'Some java arguments were not parsed',
+    'branch.not.mainline': 'The branch is not mainline',
+    'java.runtime.wrong.source': 'Unexpected source for the java runtime !',
+    'java.runtime.wrong.type': 'The runtime is not a JRE !',
+    'renderer.unsupported.arch': 'Unsupported renderer on 32 bits !',
+    'install.forge.corrupted': 'Corrupted forge install detected',
+    'load.shader.fail': 'Some shaders failed to be loaded'
+}
+
+
+def localize(localizable_array: list[str]) -> list[str]:
+    """When the request contains it, display a plain english version of the text"""
+    return list(map(lambda x: en_locale.get(x, x), localizable_array))
